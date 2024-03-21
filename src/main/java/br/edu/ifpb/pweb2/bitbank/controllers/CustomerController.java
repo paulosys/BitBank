e1f4407af04a184f2e7888bfcfe9aa646f0df8f7
paulosys <paulosilvasergio486@gmail.com>
2024-03-21 16:10:36 -0300

    feat: Refactor CustomerController
    - Using ModelAndView in all methods
    - Using redirect when saving customer

--- src/main/java/br/edu/ifpb/pweb2/bitbank/controllers/CustomerController.java
+++ src/main/java/br/edu/ifpb/pweb2/bitbank/controllers/CustomerController.java
@@ -4,8 +4,10 @@ import br.edu.ifpb.pweb2.bitbank.models.Customer;
 import br.edu.ifpb.pweb2.bitbank.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 @RequestMapping("/customers")
@@ -14,16 +16,25 @@ public class CustomerController {
     @Autowired
     private CustomerService customerService;
 
-    @RequestMapping("/form")
-    public String getForm(Customer customer, Model model){
-        model.addAttribute("customer", customer);
-        return "customers/form";
+    @GetMapping("/form")
+    public ModelAndView getForm(Customer customer, ModelAndView modelAndView){
+        modelAndView.addObject("customer", customer);
+        modelAndView.setViewName("customers/form");
+        return modelAndView;
     }
 
-    @RequestMapping("/save")
-    public String save(Customer customer, Model model){
+    @PostMapping("/save")
+    public ModelAndView save(Customer customer, ModelAndView modelAndView){
         customerService.save(customer);
-        model.addAttribute("customers", customerService.findAll());
-        return "customers/list";
+        modelAndView.addObject("customers", customerService.findAll());
+        modelAndView.setViewName("redirect:/customers/list");
+        return modelAndView;
+    }
+
+    @GetMapping("/list")
+    public ModelAndView list(ModelAndView modelAndView){
+        modelAndView.addObject("customers", customerService.findAll());
+        modelAndView.setViewName("customers/list");
+        return modelAndView;
     }
 }
